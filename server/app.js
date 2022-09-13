76fb5a4e48398faf18b733619e59a8671469a95c
ranel <binijuancruz@gmail.com>
2022-09-13 16:07:09 -0300

    Cambios en la documentacion

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
+// requerimos el modulo express
 const express = require('express')
-
+// instanciamos express
+const app = express()
 
 // config
-const app = express()
 const PORT = process.env.PORT || 3000
+
+/* La función express.urlencoded() es una función de middleware integrada en Express. 
+Analiza las solicitudes entrantes con cargas útiles codificadas en urlencoded
+y se basa en el bodyparser.
+
+ * Permite acceder a la
+información del cuerpo de dicha petición.*/
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
